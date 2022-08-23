8b6b6f5267facdb39cba5217a7d5a66706f5d9df
tenmark86 <tenmark86@gmail.com>
2022-08-23 05:07:09 -0700

    function that inserts a node as the right-child of another node

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_insert_left - inserts a node as the left-child of another node
+ *binary_tree_insert_right - inserts a node as the right-child of another node
  *@parent: pointer to the parent node of the node to create
  *@value: is the value to put in the new node
  *Return: poiter to the new node or NULL in failure
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *temp = NULL;
 
@@ -14,6 +14,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	temp = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+
+	if (!value)
+		return (NULL);
 
 	if (temp == NULL)
 		return (NULL);
@@ -23,17 +26,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	temp->right = NULL;
 	temp->parent = NULL;
 
-	if (parent->left != NULL)
+	if (parent->right != NULL)
 	{
-		temp->left = parent->left;
-		parent->left->parent = temp;
-		parent->left = temp;
+		temp->right = parent->right;
+		parent->right->parent = temp;
+		parent->right = temp;
 		temp->parent = parent;
 	}
 	else
 	{
 		temp->parent = parent;
-		parent->left = temp;
+		parent->right = temp;
 	}
 	return (temp);
 }
